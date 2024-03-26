e791184262751fbe722760f8eafd520348a6c18c
AliciaoE <alialioh@gmail.com>
2024-03-26 16:10:03 +0100

    add image

--- client/src/app/components/Footer.jsx
+++ client/src/app/components/Footer.jsx
@@ -1,13 +1,14 @@
+import Image from "next/image";
 import React from 'react';
 
 function Footer() {
   return (
     <footer className="footer">
  <Image
-          src="/image/Home-icon.svg"
+          src="/assets/image/Logo_Blanco.svg"
           alt="Icono home"
-          width={30}
-          height={30}
+          width={80}
+          height={80}
         />
    
     </footer>

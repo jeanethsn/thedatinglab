01b33e47888f33888a471b6a845ca4253e3f8e52
HemiCastillo <kirabley.hca@gmail.com>
2024-03-30 20:56:01 +0100

    folder structure modification

--- client/src/app/components/modal/LoginContent.jsx
+++ client/src/app/components/modal/LoginContent.jsx
@@ -1,45 +1,45 @@
-"use client"
-import React, { useState } from 'react';
-// import api from './api';
+// "use client"
+// import React, { useState } from 'react';
+// // import api from './api';
 
-const LoginContent = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+// const LoginContent = () => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [error, setError] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
 
-    try {
-      const response = await api.post('/login', {
-        email,
-        password,
-      });
+//     try {
+//       const response = await api.post('/login', {
+//         email,
+//         password,
+//       });
 
-      //mostrar mensaje de éxito
-      console.log(response);
+//       //mostrar mensaje de éxito
+//       console.log(response);
 
-    } catch (error) {
-      setError(error.response.data.message);
-    }
-  };
+//     } catch (error) {
+//       setError(error.response.data.message);
+//     }
+//   };
 
-  return (
-    <div className="login-content">
-      <form onSubmit={handleSubmit}>
-        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <br />
-        <br />
-        <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
-        {error && <p className="error-message">{error}</p>}
+//   return (
+//     <div className="login-content">
+//       <form onSubmit={handleSubmit}>
+//         <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+//         <br />
+//         <br />
+//         <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+//         {error && <p className="error-message">{error}</p>}
 
-        <br />
-        <br />
+//         <br />
+//         <br />
 
-        <button type="submit">Iniciar sesión</button>
+//         <button type="submit">Iniciar sesión</button>
        
-      </form>
-    </div>
-  );
-};
+//       </form>
+//     </div>
+//   );
+// };
 

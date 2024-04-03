12f5d42af6cc1315b5ee07255722e2ac98118339
debora-smb <debora.smborges@gmail.com>
2024-04-03 22:39:18 +0200

    finished style match page

--- client/src/app/components/InfoUser.jsx
+++ client/src/app/components/InfoUser.jsx
@@ -1,21 +1,23 @@
 export default function InfoUser() {
     return (
         <>
-            <div>
-                <h3 className="lg:text-nowrap text-center lg:text-left text-xl font-bold text-red-orange mb-4">
+            <div className="mb-12">
+                <h3 className="lg:text-nowrap text-left text-xl font-bold text-red-orange mb-4">
                     Sobre mí
                 </h3>
                 <p className="text-left font-nunito text-base">
                     En mi tiempo libre, disfruto de actividades al aire libre como caminatas en la naturaleza o días en la playa, pero también aprecio las noches acogedoras en casa viendo películas o leyendo un buen libro. Soy una persona creativa y me encanta explorar diferentes formas de expresión artística, ya sea a través de la pintura, la escritura o la música.
                 </p>
+                <hr className="lg:hidden w-[10rem] my-8 m-auto border-red-orange"></hr>
             </div>
             <div>
-                <h3 className="lg:text-nowrap text-center text-xl lg:text-left font-bold text-red-orange mb-4">
+                <h3 className="lg:text-nowrap text-xl text-left font-bold text-red-orange mb-4">
                     Mis intereses
                 </h3>
                 <p className="text-left font-nunito text-base">
                     En mi tiempo libre, disfruto de actividades al aire libre como caminatas en la naturaleza o días en la playa, pero también aprecio las noches acogedoras en casa viendo películas o leyendo un buen libro. Soy una persona creativa y me encanta explorar diferentes formas de expresión artística, ya sea a través de la pintura, la escritura o la música.
                 </p>
+                <hr className="lg:hidden w-[10rem] my-8 m-auto border-red-orange"></hr>
             </div>
         </>
     );

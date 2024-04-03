ba48e5e22a2e16ed4083a8cb044f9f9fa80cf09b
annaLucian <garielapoves01@gmail.com>
2024-04-03 23:01:21 +0200

    improvements hero

--- client/src/app/components/Hero.jsx
+++ client/src/app/components/Hero.jsx
@@ -1,9 +1,10 @@
 "use client";
 import { useUser } from "@/app/providers/UserProvider";
 import Button from "@/app/components/Button.jsx";
+import Image from "next/image";
 export default function Hero() {
   const { user } = useUser();
-  console.log({ user });
+
   return (
     <div className="flex flex-wrap justify-center z-[1] relative lg:w-full lg:justify-none">
       <div className="h-[20rem] sm:h-[30rem] lg:basis-[50%] lg:h-[30rem] xl:h-[40rem] xl:pt-[2rem] xxl:h-[45rem]">
@@ -40,13 +41,23 @@ export default function Hero() {
           </>
         ) : (
           <>
-            <h1 className="font-madi text-primary-color  text-[2rem] sm:text-[2.8rem] xl:text-[3.8rem]">
-              ¡Bienvenido de nuevo!
+            <Image
+              src={"/assets/icon/icon-userlogged.svg"}
+              width={80}
+              height={50}
+              alt="icono de corazones"
+              className="sm:w-[5.5rem] h-auto md:w-[6.5rem] lg:w-[7.5rem]"
+            />
+            <h1 className="leading-[2.1rem] text-primary-color  text-[1.2rem] sm:text-[1.6rem] sm:leading-[2.5rem] lg:leading-[3rem] xl:text-[2.1rem] xl:leading-[3.5rem]">
+              ¡Nos alegra verte de nuevo!{" "}
+              <span className="font-madi text-primary-color  text-[2.6rem] sm:text-[3.5rem] lg:text-[4rem] xl:text-[4.5rem]">
+                {user.name}
+              </span>
             </h1>
-            <p className="font-bold text-[1.2rem] leading-[1.6rem] sm:text-[2rem] sm:leading-[2.2rem] xl:text-[3rem] xl:leading-[3.2rem]">
-              ¿Listo para desafiar las expectativas?{" "}
+            <p className="font-bold text-[1.1rem] leading-[1.4rem] pt-[0.5rem] sm:pt-[1rem] sm:text-[1.2rem] sm:leading-[2.2rem] lg:pt-[0.5rem] xl:text-[1.5rem] xl:leading-[3.2rem]">
+              ¿Quieres desafiar tus expectativas?{" "}
             </p>
-            <p className="text-start pt-[0.5rem] text-[0.8rem] font-semibold leading-[1.1rem] sm:text-[1.1rem] sm:leading-[1.4rem] sm:pt-[1rem] xl:text-[1.2rem] xl:leading-[1.6rem]">
+            <p className="text-center pt-[0.5rem] text-[0.8rem] sm:pt-0 font-semibold leading-[1.1rem] sm:text-[1.1rem] sm:leading-[1.4rem]  xl:text-[1.2rem] xl:leading-[1.6rem]">
               Únete a nuestros eventos a ciegas y deja que la química hable por
               sí misma...
             </p>

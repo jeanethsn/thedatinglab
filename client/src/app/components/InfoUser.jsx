94ae95b1e1c1e937b5b6208c6192217e4d3efdf8
debora-smb <debora.smborges@gmail.com>
2024-04-03 22:07:49 +0200

    update info user component

---
+++ client/src/app/components/InfoUser.jsx
@@ -0,0 +1,22 @@
+export default function InfoUser() {
+    return (
+        <>
+            <div>
+                <h3 className="lg:text-nowrap text-center lg:text-left text-xl font-bold text-red-orange mb-4">
+                    Sobre mí
+                </h3>
+                <p className="text-left font-nunito text-base">
+                    En mi tiempo libre, disfruto de actividades al aire libre como caminatas en la naturaleza o días en la playa, pero también aprecio las noches acogedoras en casa viendo películas o leyendo un buen libro. Soy una persona creativa y me encanta explorar diferentes formas de expresión artística, ya sea a través de la pintura, la escritura o la música.
+                </p>
+            </div>
+            <div>
+                <h3 className="lg:text-nowrap text-center text-xl lg:text-left font-bold text-red-orange mb-4">
+                    Mis intereses
+                </h3>
+                <p className="text-left font-nunito text-base">
+                    En mi tiempo libre, disfruto de actividades al aire libre como caminatas en la naturaleza o días en la playa, pero también aprecio las noches acogedoras en casa viendo películas o leyendo un buen libro. Soy una persona creativa y me encanta explorar diferentes formas de expresión artística, ya sea a través de la pintura, la escritura o la música.
+                </p>
+            </div>
+        </>
+    );
+}

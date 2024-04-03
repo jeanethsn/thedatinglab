60add44ce1d7f6166551820b9294cf0bd3860d99
stef-ania <stebofof@gmail.com>
2024-04-03 21:57:17 +0200

    Updated PreferencesForm, still WIP

--- client/src/app/components/PreferencesForm.jsx
+++ client/src/app/components/PreferencesForm.jsx
@@ -20,12 +20,16 @@ const PreferencesForm = () => {
     values1: "",
     values2: "",
     values3: "",
+    prefers1: "",
+    prefers2: "",
+    catsDogs: "",
     rrss: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+    console.log(e);
   };
 
   const handleSubmit = async (e) => {
@@ -45,6 +49,9 @@ const PreferencesForm = () => {
         values1: response.values1,
         values2: response.values2,
         values3: response.values3,
+        prefers1: response.prefers1,
+        prefers2: response.prefers2,
+        catsDogs: response.prefers2,
         rrss: response.rrss,
       });
 
@@ -64,19 +71,143 @@ const PreferencesForm = () => {
       </h3>
       <form onSubmit={handleSubmit}>
         <div className="mb-[1rem]">
-          <label className="text-[#545454] font-nunito font-bold text-[1rem] leading-[0rem]">Fecha de nacimiento</label>
-          <Input name="birthdate" type="date" value={formData.birthdate} onChange={handleChange} required />
+          <label className="text-[#545454] font-nunito font-bold text-[1rem] leading-[0rem]">
+            Introduce tu fecha de nacimiento
+          </label>
+          <Input name="birthdate" type="date" value={formData.birthdate} onChange={handleChange} />
         </div>
 
         <div className="mb-[1rem]">
-          <label className="text-[#545454] font-nunito font-bold text-[1rem] leading-[0rem]">Género</label>
+          <label className="text-[#545454] font-nunito font-bold text-[1rem] leading-[0rem]">
+            ¿Con qué género te identificas?
+          </label>
           <Select name="gender" label={formData.gender} value={formData.gender} onChange={handleChange} required>
-            <Option name="gender">mujer</Option>
-            <Option name="gender">hombre</Option>
-            <Option name="gender">no binario</Option>
-            <Option name="gender">todo</Option>
+            <Option value="mujer">Mujer</Option>
+            <Option value="hombre">Hombre</Option>
+            <Option value="no binario">No binario</Option>
           </Select>
         </div>
+
+        <div className="mb-[1rem]">
+          <label className="text-[#545454] font-nunito font-bold text-[1rem] leading-[0rem]">
+            Estoy interesada/o en conocer a...
+          </label>
+          <Select name="looksFor" label={formData.looksFor} value={formData.looksFor} onChange={handleChange}>
+            <Option value="mujer">Mujer</Option>
+            <Option value="hombre">Hombre</Option>
+            <Option value="no binario">Personas no binarias</Option>
+            <Option value="todo">Todo</Option>
+          </Select>
+        </div>
+
+        <div className="mb-[1rem]">
+          <label className="text-[#545454] font-nunito font-bold text-[1rem] leading-[0rem]">
+            Rango de edad deseado:
+          </label>
+          <Select name="ageRange" label={formData.ageRange} value={formData.ageRange} onChange={handleChange}>
+            <Option value="20-30">De 20 a 30 años</Option>
+            <Option value="25-35">De 25 a 35 años</Option>
+            <Option value="35-45">De 35 a 45 años</Option>
+            <Option value="no importa">La edad no es un criterio para mí</Option>
+          </Select>
+        </div>
+
+        <div className="mb-[1rem]">
+          <label className="text-[#545454] font-nunito font-bold text-[1rem] leading-[0rem]">
+            ¿Qué tipo de relación sexo afectiva te gustaría tener?
+          </label>
+          <Select
+            name="sexoAffective"
+            label={formData.sexoAffective}
+            value={formData.sexoAffective}
+            onChange={handleChange}
+          >
+            <Option value="monogama">Una relación monógama</Option>
+            <Option value="explorar">Una relación monógama en la que explorar</Option>
+            <Option value="abierta">Una relación abierta, poliamorosa, etc...</Option>
+            <Option value="beneficios">Amig@s con beneficios</Option>
+            <Option value="fluir">Lo que fluya, estoy abiert@ a cualquier formato</Option>
+            <Option value="casual">Algo totalmente casual</Option>
+          </Select>
+        </div>
+
+        <div className="mb-[1rem]">
+          <label className="text-[#545454] font-nunito font-bold text-[1rem] leading-[0rem]">
+            ¿En qué estado se encuentra tu corazón?
+          </label>
+          <Select name="heartState" label={formData.heartState} value={formData.heartState} onChange={handleChange}>
+            <Option value="maduro">
+              Maduro y sereno y dispuesto a compartirlo. Con alegrías y traumas procesados e integrados
+            </Option>
+            <Option value="solo">Un poco solito</Option>
+            <Option value="feliz">Feliz y palpitante con ganas de conocer a personas</Option>
+            <Option value="recuperarse">Acabo de salir de una relación y busco recuperarme y distraerme</Option>
+            <Option value="despechado">Más despechado que Shakira y Piqué</Option>
+          </Select>
+        </div>
+
+        <div className="mb-[1rem]">
+          <label className="text-[#545454] font-nunito font-bold text-[1rem] leading-[0rem]">¿Tienes hijos?</label>
+          <div className="flex flex-wrap -mx-2 mb-2">
+            <div className="w-1/2 px-2 mb-2">
+              <label>
+                <input type="radio" name="hasChildren" value="si" onChange={handleChange} required />
+                <span className="ml-2">Sí</span>
+              </label>
+            </div>
+            <div className="w-1/2 px-2 mb-2">
+              <label>
+                <input type="radio" name="hasChildren" value="no" onChange={handleChange} required />
+                <span className="ml-2">No</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-[1rem]">
+          <label className="text-[#545454] font-nunito font-bold text-[1rem] leading-[0rem]">
+            ¿Saldrías con alguien que tiene hijos?
+          </label>
+          <div className="flex flex-wrap -mx-2 mb-2">
+            <div className="w-1/2 px-2 mb-2">
+              <label>
+                <input type="radio" name="datesParents" value="si" onChange={handleChange} required />
+                <span className="ml-2">Sí</span>
+              </label>
+            </div>
+            <div className="w-1/2 px-2 mb-2">
+              <label>
+                <input type="radio" name="datesParents" value="no" onChange={handleChange} required />
+                <span className="ml-2">No</span>
+              </label>
+            </div>
+            <div className="w-1/2 px-2 mb-2">
+              <label>
+                <input type="radio" name="datesParents" value="no sabe" onChange={handleChange} required />
+                <span className="ml-2">No me lo he planteado</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-[1rem]">
+          <label className="text-[#545454] font-nunito font-bold text-[1rem] leading-[0rem]">
+            Indica un valor importante para ti entre estos:
+          </label>
+          <Select name="values1" label={formData.values1} value={formData.values1} onChange={handleChange}>
+            <Option value="amabilidad">Amabilidad</Option>
+            <Option value="amistad">Amistad</Option>
+            <Option value="autenticidad">Autenticidad</Option>
+            <Option value="aventura">Aventura</Option>
+            <Option value="comunicacion">Comunicacion</Option>
+            <Option value="conciencia">Conciencia</Option>
+            <Option value="confianza">Confianza</Option>
+            <Option value="creatividad">Creatividad</Option>
+            <Option value="cuidado">Cuidado</Option>
+            <Option value="desarrollo">Desarrollo</Option>
+          </Select>
+        </div>
+
         <Button
           color="secondary"
           type="submit"

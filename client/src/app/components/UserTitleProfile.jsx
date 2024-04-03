9a6d2179a7743d75233784b381c03018dd4b4ddd
debora-smb <debora.smborges@gmail.com>
2024-04-03 15:43:25 +0200

    finished menu and basic info profile components

--- client/src/app/components/UserTitleProfile.jsx
+++ client/src/app/components/UserTitleProfile.jsx
@@ -2,23 +2,23 @@ import Image from "next/image";
 
 export default function UserTitleProfile(){
     return(
-        <div className="flex flex-col-reverse lg:flex-row gap-4 py-4">
+        <div className="flex flex-col-reverse lg:flex-row gap-4 py-4 items-center justify-between">
             <section className="flex flex-col gap-2 text-red-orange">
                 <h6 className="text-base">
                     Datos de la cuenta
                 </h6>
-                <h2 className="text-3xl font-black">
+                <h2 className="text-3xl font-black lg:text-6xl">
                     <span className="text-grey-dark">Hola, </span>
                     Usuario
                 </h2>
             </section>
-            <div className="m-auto w-[200px] h-[200px] rounded-full overflow-hidden lg:w-[365px] lg:h-[365px]">
+            <div className="m-auto w-[12.5rem] h-[12.5rem] rounded-full overflow-hidden lg:m-0 lg:w-[14rem] lg:h-[14rem]">
                 <Image
                     src="/assets/image/imagen-hero.png"
                     width={200}
                     height={200}
                     alt="User photo"
-                    className="object-fill lg:w-[3.65rem] lg:h-[365rem]"
+                    className="object-fill"
                 />
             </div>
         </div>

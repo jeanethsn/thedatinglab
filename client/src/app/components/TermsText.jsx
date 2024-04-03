bc46103ab34ca5c3c1b9ffb2e74e7cf77fad1818
debora-smb <debora.smborges@gmail.com>
2024-04-03 19:25:43 +0200

    update terms page mobile version

--- client/src/app/components/TermsText.jsx
+++ client/src/app/components/TermsText.jsx
@@ -2,14 +2,14 @@ export default function TermsText({title, text}) {
     return (
 
         <section className="flex flex-col gap-4">
-            <div className='flex flex-row items-center'>
-                <h3 className="text-nowrap text-xl font-bold text-red-600">{title}</h3>
+            <div className="flex flex-row items-center m-auto lg:m-0">
+                <h3 className="lg:text-nowrap text-xl font-bold text-red-600">{title}</h3>
                 <span className="hidden lg:flex ml-12 w-full border-t border-red-600"></span>
             </div>           
             <p className="text-left font-nunito text-base">
                 {text}
             </p>
-            <span className="ml-12 w-[100px] lg:hidden border-t border-red-600"></span>
+            <span className="lg:hidden border-t border-red-600"></span>
         </section>
     );
 }
--- client/src/app/terms/page.jsx
+++ client/src/app/terms/page.jsx
@@ -4,9 +4,9 @@ import TermsService from '../components/TermsText';
 export default function TermsPage() {
     return (
         <main className="gap-12 px-[10%] py-[4%] flex flex-col text-center">
-            <h1 className="text-4xl font-bold text-red-600 mb-8">Términos del Servicio</h1>
-            <TermsService title="Hola" text="Hola Mundo"/>
-            <TermsService title="Adios" text="Hola Mundo"/>
+            <h1 className="text-4xl font-bold text-red-600 lg:mb-8">Términos del Servicio</h1>
+            <TermsService title="Holacdv vdsg" text="Hola Mundo"/>
+            <TermsService title="Adiosvds" text="Hola Mundovsvas vdv fsgvawgv vdgaveger egaegaewg egewgerwg gsgsegv esgsge gegd egwerg"/>
         </main>
     );
 }

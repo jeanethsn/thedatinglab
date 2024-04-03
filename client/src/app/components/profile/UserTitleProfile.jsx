044354f40a8018ea6fa71927a6dfd42466caf73c
debora-smb <debora.smborges@gmail.com>
2024-04-03 21:13:09 +0200

    finished profile content page style

--- client/src/app/components/profile/UserTitleProfile.jsx
+++ client/src/app/components/profile/UserTitleProfile.jsx
@@ -12,7 +12,7 @@ export default function UserTitleProfile(){
                     Usuario
                 </h2>
             </section>
-            <div className="m-auto w-[12.5rem] h-[12.5rem] rounded-full overflow-hidden lg:m-0 lg:w-[14rem] lg:h-[14rem]">
+            <div className="m-auto w-[12.5rem] lg:mb-4 h-[12.5rem] rounded-full overflow-hidden lg:m-0 lg:w-[14rem] lg:h-[14rem]">
                 <Image
                     src="/assets/image/imagen-hero.png"
                     width={200}

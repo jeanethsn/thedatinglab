eb649ff33ad2c98f441cf20cf7f262c30b546f90
AliciaoE <alialioh@gmail.com>
2024-04-04 23:08:07 +0200

    modificate stiles

--- client/src/app/(user)/event/[id]/page.jsx
+++ client/src/app/(user)/event/[id]/page.jsx
@@ -2,13 +2,12 @@
 import { useParams } from "next/navigation";
 import { useEffect, useState } from 'react';
 import Image from "next/image";
-import Button from "@/app/components/Button.jsx";
 import {getEventById} from "@/app/services/event.js";
 import Statement from "@/app/components/Statement.jsx";
+import Button from "@/app/components/Button";
 
 function page() {
     const params = useParams();
-    
     const [event, setEvent] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
   
@@ -31,19 +30,23 @@ function page() {
     return (
         <>
         <Statement/>
-    <div className="bg-pink-cream gap-10 flex justify-center items-center flex-col border-t-2 border-primary-color  ">
-    <div className=" flex flex-col justify-center mt-[2rem] items-center md:flex md:flex-row w-full md:gap-[8rem]">
-    <div className=" md:mb-[2rem] flex justify-center items-center w-[22rem] h-[22rem]  md:w-[25rem] md:h-[25rem]" 
-    style={{ backgroundImage: `url(http://localhost:8000/storage/${event.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div className="bg-pink-cream gap-10 flex justify-center items-center flex-col border-t-2 border-primary-color md:mb-[3rem] ">
+    <div className=" flex flex-col justify-center items-center mt-[4rem] md:flex md:flex-row w-full md:gap-[8rem] md:mt-[5rem]">
+    <div className=" rounded-md flex justify-center items-center w-[22rem] h-[22rem] mb-[2rem] md:w-[25rem] md:h-[22rem] md:mb-[2rem] " 
+    style={{ backgroundImage: `url(http://localhost:8000/storage/${event.image})`, backgroundSize: 'cover',  }}>
     </div>
-    <div className="m-[1rem] md:w-[45%] md:flex md:flex-col items-center md:items-start max-w-md">
-      <h1 className="leading-tight font-bold text-[2rem] text-left mb-[1rem]"> Cata de Vinos</h1>
-      <p className="text-[1.10rem] font-bold text-justify mb-[1rem]">¡Descubre el poder de la conexión real a través del fascinante mundo de los vinos!</p>
-      <p className="text-primary-color font-bold text-[1.5rem] mb-[1rem] ">DOM, 17 SEPT-19:00</p>
-      <p className="text-[1rem] mb-[1rem]">¡Sumérgete en la emoción de descubrir nuevos sabores sin prejuicios preconcebidos! En esta experiencia única, te emparejaremos con vinos compatibles con tus gustos emocionales para que disfrutes de una cata a ciegas llena de sorpresas y deleite para tus sentidos.  </p>
-      <p className="text-[1rem] flex items-center gap-[0.5rem]"> <span> <img src="/assets\icon\icon-location.svg" alt="Logo" className="h-4 w-auto" /> </span> Barcelona</p>
+    <div className=" m-[1rem] md:w-[45%] md:flex md:flex-col items-center md:items-start ">
+      <h1 className="leading-tight font-bold text-[2rem] text-left mb-[1rem]"> {event.title}</h1>
+      <p className="text-[1.10rem] font-semibold text-justify mb-[1rem] ">{event.shortDescription}</p>
+      <p className="text-primary-color font-bold text-[1.5rem] mb-[1rem]  "> {event.date}</p>
+      <p className="text-[1rem] mb-[1rem]"> {event.description} </p>
+      <p className="text-[1rem] flex items-center gap-[0.5rem]"> <span> <img src="/assets\icon\icon-location.svg" alt="Logo" className="h-4 w-auto" /> </span> {event.location}</p>
+
+      
+  <Button/>
     </div>
   </div>
+
 </div>
 
 
--- client/src/app/components/Statement.jsx
+++ client/src/app/components/Statement.jsx
@@ -4,9 +4,9 @@ export default function Statement() {
   return (
     <>
 <div className=" hidden md:block bg-red-600 text-center text-white leading-loose pb-[0.50rem]">
-    <h1 className=" text-[2.5rem] font-madi ">
+    <h1 className=" text-[3rem] font-madi ">
       ¿ Y ahora que ?</h1>
-      <p className="text-[0.75rem] ">¿Te hemos preparado distintos eventos para que conozcas a tus matches!<br />
+      <p className="text-[0.90rem] ">¿Te hemos preparado distintos eventos para que conozcas a tus matches!<br />
    ¡Toca cada evento para ver todos los detalles! ¡Suerte!</p>
 
   

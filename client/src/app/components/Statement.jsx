20d3cd7abe652e859d032e0884aa0b43fb9d5d26
AliciaoE <alialioh@gmail.com>
2024-04-04 23:32:31 +0200

    merge page event

--- client/src/app/components/Statement.jsx
+++ client/src/app/components/Statement.jsx
@@ -3,11 +3,11 @@
 export default function Statement() {
   return (
     <>
-<div className=" hidden md:block bg-red-600 text-center text-white leading-loose pb-[0.50rem]">
+<div className=" hidden md:block bg-red-600 text-center text-white leading-loose pb-[1rem] ">
     <h1 className=" text-[3rem] font-madi ">
       ¿ Y ahora que ?</h1>
-      <p className="text-[0.90rem] ">¿Te hemos preparado distintos eventos para que conozcas a tus matches!<br />
-   ¡Toca cada evento para ver todos los detalles! ¡Suerte!</p>
+      <p className="text-[0.90rem] ">¡Te hemos preparado distintos eventos para que conozcas a tus matches!<br />
+   ¿A qué esperas para descubrirlos?</p>
 
   
     </div>

a8a4ef44d2ee9246c66d78bdfe1b84e0241109f4
Jasz-17 <jaszapata1983@gmail.com>
2024-04-04 11:28:31 +0200

    creating card match styles

--- client/src/app/components/profile/CardMatch.jsx
+++ client/src/app/components/profile/CardMatch.jsx
@@ -1,7 +1,12 @@
-export default function CardMatch() {
+export default function CardMatch({ match }) {
+    const { name, imageSrc } = match;
+
     return (
-      <section>
-        Hola
-      </section>
+        <section className="bg-white rounded-lg shadow-md  w-64">
+            <div className= "overflow-hidden w-full h-64 rounded-t-lg">
+                <img className=" object-cover w-full h-full blur-sm" src={imageSrc} alt={`Imagen de ${name}`} />
+            </div>
+            <p className="font-semibold text-lg p-4">{name}</p>
+        </section>
     );
-  }
+}

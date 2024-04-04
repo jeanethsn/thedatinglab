dd4655e19a907e011675b57e3fedfff83d5b1c16
debora-smb <debora.smborges@gmail.com>
2024-04-04 11:25:25 +0200

    add blur to user photo

--- client/src/app/components/MatchTitleProfile.jsx
+++ client/src/app/components/MatchTitleProfile.jsx
@@ -6,13 +6,13 @@ export default function MatchTitleProfile(){
                 <h2 className="text-3xl font-black lg:text-6xl text-red-orange">
                     Usuario
                 </h2>
-            <div className="m-auto w-[12.5rem] lg:mb-4 h-[12.5rem] rounded-full overflow-hidden lg:m-0 lg:w-[14rem] lg:h-[14rem]">
+            <div className="m-auto w-[12.5rem] lg:mb-4 h-[12.5rem] rounded-full overflow-hidden lg:m-0 lg:w-[14rem] blur-sm lg:h-[14rem]">
                 <Image
                     src="/assets/image/imagen-hero.png"
                     width={200}
                     height={200}
                     alt="User photo"
-                    className="object-fill"
+                    className="object-fill "
                 />
             </div>
         </div>

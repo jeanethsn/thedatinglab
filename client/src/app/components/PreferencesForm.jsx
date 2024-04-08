c51a76283f55a387ddf7ca20fae529d737c26d03
stef-ania <stebofof@gmail.com>
2024-04-08 22:34:55 +0200

    Removed custom toast duration

--- client/src/app/components/PreferencesForm.jsx
+++ client/src/app/components/PreferencesForm.jsx
@@ -165,7 +165,6 @@ const PreferencesForm = () => {
               backgroundColor: "#FBF6F3",
               border: "1px solid #E27A8C",
             },
-            duration: 4000,
           });
           return;
         }

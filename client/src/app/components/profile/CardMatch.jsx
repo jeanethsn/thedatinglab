a93ac4d084905feacd153793a9771543c1dcb064
debora-smb <debora.smborges@gmail.com>
2024-04-09 13:17:18 +0200

    update image url

--- client/src/app/components/profile/CardMatch.jsx
+++ client/src/app/components/profile/CardMatch.jsx
@@ -1,10 +1,10 @@
 export default function CardMatch({ match }) {
-    const { name, image } = match;
+    const { name} = match;
 
     return (
         <a href="#" className="bg-white rounded-lg shadow-md w-64">
             <div className="overflow-hidden w-full h-64 rounded-t-lg relative z-1">
-                <img className="object-cover w-full h-full blur-sm" src={image} alt={`Imagen de ${name}`} />
+                <img className="object-cover w-full h-full blur-sm" src={`http://127.0.0.1:8000/storage/${user.profile_image}`} alt={`Imagen de ${name}`} />
             </div>
             <p className="font-semibold text-lg p-4 relative z-30">{name}</p>
         </a>

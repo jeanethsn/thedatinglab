61883b0ee20a6eae176a5ba46cad754f095533ba
Jasz-17 <jaszapata1983@gmail.com>
2024-04-10 12:04:40 +0200

    update styles terms and conditions

--- client/src/app/components/TermsText.jsx
+++ client/src/app/components/TermsText.jsx
@@ -2,7 +2,7 @@ export default function TermsText({ title, text }) {
   return (
     <section className="flex flex-col gap-4">
       <div className="flex flex-row items-center m-auto lg:m-0">
-        <h3 className="lg:text-nowrap text-xl font-bold text-red-600 ol:text-[1.4rem]">
+        <h3 className="text-start lg:text-nowrap text-xl font-bold text-red-600 ol:text-[1.4rem]">
           {title}
         </h3>
         <span className="hidden lg:flex ml-12 w-full border-t border-red-600"></span>
